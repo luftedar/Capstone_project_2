b440ad59cf7d64ead04b4ef6a0d74671d47fd042
unknown <yisehakmitiku@gmail.com>
2021-11-16 18:20:40 +0800

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -3,27 +3,22 @@ import axios from 'axios';
 const div = document.querySelector('.movies');
 
 const render = (movie) => {
-      div.innerHTML += `
-      <div>
-      <img src="${movie.image.medium}" alt="${movie.name}">
-      <p>${movie.name}</p>
-      <button id="${movie.id}"><i class="far fa-heart"></i></button>
-      <button>Comment</button>
-    </div>`;
-}
+  div.innerHTML += `
+  <div>
+  <img src="${movie.image.medium}" alt="${movie.name}">
+  <p>${movie.name}</p>
+  <button id="${movie.id}"><i class="far fa-heart"></i></button>
+  <button>Comment</button>
+</div>`;
+};
 
 const getData = async () => {
-  
   const movies = await axios.get('https://api.tvmaze.com/shows?limit=5');
-
-
   movies.data.forEach((movie, index) => {
     if (index < 6) {
       render(movie);
-      console.log(movie);
-
     }
   });
-}
+};
 
 getData();
