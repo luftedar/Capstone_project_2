cd26ea899a9a8fa50aea5a0c82d9969b5f0b7d65
luftedar <ugurorcun91@gmail.com>
2021-11-18 00:56:34 +0300

    Fixed Linters

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
 
 const div = document.querySelector('.movies');
-let comments = document.querySelector('.comments');
+const comments = document.querySelector('.comments');
 const body = document.querySelector('body');
 let movies = [];
 let commentHTML = '';
 let getCommentsFromAPI = [];
 
 const getMovies = async () => {
- const result = await axios.get('https://api.tvmaze.com/shows');
- movies = result.data;
- movies = movies.slice(0,6);
- return movies;
+  const result = await axios.get('https://api.tvmaze.com/shows');
+  movies = result.data;
+  movies = movies.slice(0, 6);
+  return movies;
 };
 
 const updateLikes = async (ele) => {
-  await axios.post(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes`,{ item_id: ele.id});
+  await axios.post('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes', { item_id: ele.id });
   displayMovie();
-}
+};
 
-const GetLikes = async () => { 
-  const res = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes/`);
+const GetLikes = async () => {
+  const res = await axios.get('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes/');
   return res.data;
-}
+};
 
-const displayMovie = async() => {
- const movies = await getMovies();
+const displayMovie = async () => {
+  const movies = await getMovies();
   const likes = await GetLikes();
   div.innerHTML = '';
   movies.forEach((movie, index) => {
-    let likeVal = likes[index] !== undefined ? likes[index].likes : 0;
+    const likeVal = likes[index] !== undefined ? likes[index].likes : 0;
     div.innerHTML += `
     <div id="${movie.id}">
     <img src="${movie.image.medium}" alt="${movie.name}">
@@ -40,38 +40,38 @@ const displayMovie = async() => {
     </div>
     <button class="comment-button">Comment</button>
     </div>`;
-  })
-}
+  });
+};
 
 displayMovie();
 
 const postNewComments = async (movieID, userName, userComment) => {
-  await axios.post(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments`, {
-    "item_id": movieID,
-    "username": userName,
-    "comment": userComment
+  await axios.post('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments', {
+    item_id: movieID,
+    username: userName,
+    comment: userComment,
   });
   showComments(movieID);
-}
+};
 
 const getComments = async (movieId) => {
-  const comments = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments?item_id=${movieId}`)
+  const comments = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments?item_id=${movieId}`);
   return comments.data;
-}
+};
 
 const showComments = async (id) => {
-  try{ getCommentsFromAPI = await getComments(id)}catch{
+  try { getCommentsFromAPI = await getComments(id); } catch {
     popUpHtml(id);
-    commentHTML = '<p>No Comments Yet</p>'
-    getCommentsFromAPI = [];
-  };
-  if(getCommentsFromAPI.length === 0){
     commentHTML = '<p>No Comments Yet</p>';
-  }else {
-    getCommentsFromAPI.forEach((i) => { commentHTML += `<p>${i.creation_date} ${i.username}: ${i.comment}</p>` });
+    getCommentsFromAPI = [];
   }
-  comments.innerHTML +=
-  `<div id='comment-area'>
+  if (getCommentsFromAPI.length === 0) {
+    commentHTML = '<p>No Comments Yet</p>';
+  } else {
+    getCommentsFromAPI.forEach((i) => { commentHTML += `<p>${i.creation_date} ${i.username}: ${i.comment}</p>`; });
+  }
+  comments.innerHTML
+  += `<div id='comment-area'>
         <h2>Comments</h2>
         ${commentHTML}
       </div>
@@ -83,41 +83,40 @@ const showComments = async (id) => {
           <button name="${id}" type="button">Submit Comment</button>
         </form>
       </div>`;
-}
+};
 
 const popUpHtml = (target) => {
   comments.innerHTML = `
     <div id='pop'>
       <i class='fas fa-times'></i>
-      <img src="${movies[parseInt(target) - 1].image.medium}" alt="${movies[parseInt(target) - 1].name}">
-      <h1>${movies[parseInt(target) - 1].name}</h1>
+      <img src="${movies[parseInt(target, 10) - 1].image.medium}" alt="${movies[parseInt(target, 10) - 1].name}">
+      <h1>${movies[parseInt(target, 10) - 1].name}</h1>
       <div id='comment-feature'>
-        <p> Rating: ${movies[parseInt(target) - 1].rating.average}</p>
-        <p> Released Date: ${movies[parseInt(target) - 1].premiered}</p>
-        <p> Genres: ${movies[parseInt(target) - 1].genres}</p>
-        <p> Language: ${movies[parseInt(target) - 1].language}</p>
+        <p> Rating: ${movies[parseInt(target, 10) - 1].rating.average}</p>
+        <p> Released Date: ${movies[parseInt(target, 10) - 1].premiered}</p>
+        <p> Genres: ${movies[parseInt(target, 10) - 1].genres}</p>
+        <p> Language: ${movies[parseInt(target, 10) - 1].language}</p>
       </div>
     </div>`;
-}
+};
 
 body.addEventListener('click', (e) => {
-  let indexID = e.target.parentNode.id 
+  const indexID = e.target.parentNode.id;
   if (e.target.className === 'comment-button') {
     popUpHtml(indexID);
     commentHTML = '';
     showComments(indexID);
-  }
-  else if (e.target.classList.contains('fa-heart')) {
-    updateLikes(e.target)
-  }else if (e.target.innerHTML === 'Submit Comment'){
-    let sentID = (e.target.name).toString();
-    let sentUserName = (e.target.parentNode.childNodes[1].value);
-    let sentUserComment = (e.target.parentNode.childNodes[3].value);
+  } else if (e.target.classList.contains('fa-heart')) {
+    updateLikes(e.target);
+  } else if (e.target.innerHTML === 'Submit Comment') {
+    const sentID = (e.target.name).toString();
+    const sentUserName = (e.target.parentNode.childNodes[1].value);
+    const sentUserComment = (e.target.parentNode.childNodes[3].value);
     commentHTML = '';
     comments.innerHTML = '';
     popUpHtml(e.target.name);
-    postNewComments(sentID,sentUserName,sentUserComment);
-  }else if (e.target.classList.contains('fa-times')) {
+    postNewComments(sentID, sentUserName, sentUserComment);
+  } else if (e.target.classList.contains('fa-times')) {
     comments.innerHTML = '';
     displayMovie();
   }
